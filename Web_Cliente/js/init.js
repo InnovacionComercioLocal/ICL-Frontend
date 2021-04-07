dc89f4b9c88a2373f3c4af2213463bfcbf59dae5
jordiboned <65189660+jordiboned@users.noreply.github.com>
2021-04-07 13:31:17 +0200

    Add files via upload

---
+++ Web_Cliente/js/init.js
@@ -0,0 +1,13 @@
+function init() {
+   document.getElementById("pagar").addEventListener('click',pay);
+}
+
+function printCarrito() {
+    test();
+}
+
+function pay() {
+   window.open("./metodos-de-pago.html","_self");
+}
+
+//https://www.w3schools.com/jsref/prop_text_value.asp
