c5ccbe798b5691057d8264ffd4140140988901e5
Thegamerexpert <50015390+Thegamerexpert@users.noreply.github.com>
2021-04-13 23:04:18 +0200

    Delete
    btn "Pagar sin registrar"

--- Web_Cliente/js/init.js
+++ Web_Cliente/js/init.js
@@ -1,5 +1,7 @@
 function init() {
-   document.getElementById("pagar").addEventListener('click',pay);
+   idRegister.addEventListener('click',pay);
+   idLogin.addEventListener('click',login);
+   idFinalizarPago.addEventListener('click',login);
 }
 
 function printCarrito() {
@@ -10,4 +12,12 @@ function pay() {
    window.open("./metodos-de-pago.html","_self");
 }
 
+function login(email, password) {
+   alert("Is working");
+}
+
 //https://www.w3schools.com/jsref/prop_text_value.asp
+
+var idLogin = document.getElementById("login");
+var idRegister = document.getElementById("register");
+var idFinalizarPago = document.getElementById("pagar");
