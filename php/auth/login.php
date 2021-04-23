34f3dcf7699ca49c39269d8372b2e7b31ad6fad7
Thegamerexpert <50015390+Thegamerexpert@users.noreply.github.com>
2021-04-23 12:31:55 +0200

    General corrections
    Directions,
    Errors pages

--- php/auth/login.php
+++ php/auth/login.php
@@ -1,62 +1,55 @@
 <?php
 
+//Connect to BD
 include("../conexionBD.php");
 
+//Get var value
 $email = mysqli_real_escape_string($mysqli, $_POST['email']);
 $passwordS = mysqli_real_escape_string($mysqli, $_POST['password']);
 $contador = 0;
 $password =  htmlspecialchars($passwordS, $flags = ENT_COMPAT | ENT_HTML401, $encoding = ini_get("default_charset"), $double_encode = true);
 
+//Req
 $comprobacion = $mysqli->query("SELECT * from usuario WHERE usuario.Email='$email'");
 
+//If not exists
 if (mysqli_num_rows($comprobacion) <= 0) {
-    echo ("Login no existe o Contraseña incorrecta");
-    echo 'El email o password es incorrecto, <a href="../../index.html">vuelva a intenarlo</a>.<br/>';
+    //Redirect to errors/not exists users    
+    header("location:../../errors/error-login.html");
+    
+    //If it exists
 } else {
+    //Run all array
     $row = $comprobacion->fetch_object();
 
-
+    //Chech password
     if (password_verify($password, $row->Password)) {
         $contador++;
-?>
-
-        <script type="text/javascript">
-            alert("Contraseña Correcta");
-        </script>
-
-    <?php
-
     }
-
-
+    
+    //Correct password or username
     if ($contador > 0) {
         echo ("Usuario: " . $row->Nombre . " conectado");
         $RoleUsuActive = $row->ID_Role;
+        //Start session and set var session
         session_start();
         $_SESSION['usuario'] = array();
         $_SESSION['usuario']['email'] = $email;
         $_SESSION['usuario']['ID_Role'] = $RoleUsuActive;
         print_r($_SESSION['usuario']);
 
-
+        //change status connection
         $mysqli->query("UPDATE usuario SET Validado=1 WHERE usuario.Email ='$email'");
         echo ($mysqli->error);
-        header("location:../../menus.html");
+        //redirect to products.html
+        header("location:../../productos.html");
+
+        //Incorrect password
     } else {
-        echo $password;
-    ?>
-
-        <script type="text/javascript">
-            alert("Contraseña incorrecta");
-        </script>
-
-
-<?php
-        echo 'El email o password es incorrecto, <a href="../../index.html">vuelva a intenarlo</a>.<br/>';
+        //Redirect to errors/error-login
+        header("location:../../errors/error-login.html");
     }
 }
 
-
 echo ($mysqli->error);
 $mysqli->close();
-?>
--- php/auth/signUp.php
+++ php/auth/signUp.php
@@ -8,7 +8,9 @@ $password = $_POST['password'];
 
 include("../conexionBD.php");
 $comprobacion = $mysqli->query("SELECT * from usuario WHERE usuario.Email='$email'");
-if (mysqli_num_rows($comprobacion) <= 0) {
+//If email not exist
+if (mysqli_num_rows($comprobacion) <= 0) {    
+    //Check passwords
     if ($_POST['password'] == $_POST['ConfirmarPassword']) {
         $passwordCrypt = password_hash($_POST['password'], PASSWORD_DEFAULT);
         $result = $mysqli->query("INSERT INTO usuario (Nombre,Telefono,Password,Email) VALUES ('$nombre','$telefono','$passwordCrypt','$email')");
@@ -18,11 +20,17 @@ if (mysqli_num_rows($comprobacion) <= 0) {
         $mysqli->query("UPDATE usuario SET validado=1 WHERE usuario.Email ='$email'");
         echo ($mysqli->error);
         header("location:../../index.html");
+
+        //redirect to errors/error-register.html
     } else {
+        //Create local var Error type
         echo ("<div id='Div1'><span style='color: red;' > Las Contraseñas deben ser iguales</span></div> ");
     }
+
+    //If email exist
 } else {
-    echo ("<div id='Div1'><span style='color: red;' >Este email ya esta registrado</span></div> ");
+    //redirect to errors/error-register.html
+    header("location:../../errors/error-register.html");
 }
 
 echo ($mysqli->error);
